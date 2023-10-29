7dc94b6b83c4bb473f44233aea2d818deeccf98b
GianmarcoPas <148062741+GianmarcoPas@users.noreply.github.com>
2023-10-29 10:57:23 +0100

    modifica

---
+++ comandi.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Eseguo il comando "git add ."
+git add .
+
+echo Eseguo il comando "git commit -m"
+git commit -m "modifica"
+
+echo Eseguo il comando "git push origin main"
+git push origin main
+
+echo Completato!
